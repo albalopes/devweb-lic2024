e5523d6f4fec8759fadd5171da06bbc5fdc3fe94
albalopes <alba.lopes@ifrn.edu.br>
2024-03-05 00:38:40 +0000

    Passagem de vários parâmetros e valores default

--- app.py
+++ app.py
@@ -22,9 +22,26 @@ def alunos():
 def professores():
     return render_template('professores.html')
 
+'''
+@app.route('/usuario')
+@app.route('/usuario/<nome>')
+def usuario(nome='anônimo'):
+    return render_template('usuario.html', nome=nome)
+'''
+
+@app.route('/usuario', defaults={"nome": "anônimo"})
 @app.route('/usuario/<nome>')
 def usuario(nome):
     return render_template('usuario.html', nome=nome)
 
+@app.route('/dobro', defaults={"n": 1})
+@app.route('/dobro/<int:n>')
+def dobro(n):
+    calculo = 2*n
+    return 'O dobro é: '+str(calculo)
 
-
+@app.route('/soma', defaults={'n1': 0, 'n2': 0})
+@app.route('/soma/<int:n1>/<int:n2>')
+def soma(n1, n2):
+    result = n1 + n2
+    return render_template('soma.html', a=n1, b=n2, c=result)
